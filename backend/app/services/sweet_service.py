90152bb3479ed9834b612452c08dd177b63f4bbf
ShantanuisCoding <sshantanu781@gmail.com>
2025-12-13 22:28:23 +0530

    feat: add purchase sweet business logic

--- backend/app/services/sweet_service.py
+++ backend/app/services/sweet_service.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Session
 from app.db.models import Sweet
+from fastapi import HTTPException
+
 
 def create_sweet(db: Session, data: dict):
     sweet = Sweet(**data)
@@ -10,3 +12,17 @@ def create_sweet(db: Session, data: dict):
 
 def get_all_sweets(db: Session):
     return db.query(Sweet).all()
+
+def purchase_sweet(db: Session, sweet_id: int):
+    sweet = db.query(Sweet).filter(Sweet.id == sweet_id).first()
+
+    if not sweet:
+        raise HTTPException(status_code=404, detail="Sweet not found")
+
+    if sweet.quantity <= 0:
+        raise HTTPException(status_code=400, detail="Out of stock")
+
+    sweet.quantity -= 1
+    db.commit()
+    db.refresh(sweet)
+    return sweet
