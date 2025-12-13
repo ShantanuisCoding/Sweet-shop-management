74e462ec195fda25474a2d99e5c99f9f5b97ab0d
ShantanuisCoding <sshantanu781@gmail.com>
2025-12-13 20:21:53 +0530

    feat: add sweet model

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
+
 from app.db.base import Base
 
 class User(Base):
@@ -8,3 +9,12 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     role = Column(String, default="USER")
+
+class Sweet(Base):
+    __tablename__ = "sweets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+    category = Column(String, index=True, nullable=False)
+    price = Column(Float, nullable=False)
+    quantity = Column(Integer, nullable=False)
