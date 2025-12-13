074de586fae9bf93fa708d5cfae41298e150307b
ShantanuisCoding <sshantanu781@gmail.com>
2025-12-14 01:51:44 +0530

    feat: render login page in app root

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import { AuthProvider } from "./auth/AuthContext";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <div>
+      <Login />
+    </div>
   );
 }
