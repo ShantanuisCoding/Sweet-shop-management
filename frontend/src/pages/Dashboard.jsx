0d09a537dbf1c6a18050dabf5eb4a603a380697a
ShantanuisCoding <sshantanu781@gmail.com>
2025-12-14 01:53:44 +0530

    chore: add dashboard page skeleton

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,20 +1,8 @@
-import { useEffect, useState } from "react";
-import api from "../api";
-import SweetCard from "../components/SweetCard";
-
 export default function Dashboard() {
-  const [sweets, setSweets] = useState([]);
-
-  useEffect(() => {
-    api.get("/api/sweets").then(res => setSweets(res.data));
-  }, []);
-
   return (
     <div>
-      <h2>Sweets</h2>
-      {sweets.map(s => (
-        <SweetCard key={s.id} sweet={s} onUpdate={setSweets} />
-      ))}
+      <h2>Dashboard</h2>
+      {/* sweets list will be added later */}
     </div>
   );
 }
